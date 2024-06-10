7e9e427f48004204edfe5a3c332756e13a5b7b83
dre7djib <djibril.camara.pradier@gmail.com>
2024-06-10 10:31:51 +0200

    Logger Auth Middleware

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,20 +1,22 @@
 import e, { NextFunction } from "express";
 import { JwtService } from '@nestjs/jwt';
-import { jwtConstants } from "./constants";
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 
 @Injectable()
 export class AuthMiddleware {
     constructor(private jwtService: JwtService) {}
+    private readonly logger = new Logger(AuthMiddleware.name);
+
     async use(req: Request, res: Response, next: NextFunction) {
+        this.logger.log('Checking if user is authenticated');
         const authHeader = req.headers['authorization'];
         if (authHeader && authHeader.startsWith('Bearer ')) {
             const token = authHeader.split(' ')[1];
             try {
-            const decoded = this.jwtService.verifyAsync(token, {secret: jwtConstants.secret} );
+            const decoded = await this.jwtService.verifyAsync(token, { secret: process.env.JWT_SECRET });
             req['user'] = decoded;
             } catch (error) {
-                return error;
+                throw new Error('Invalid token');
             }
         } else {
             return
--- src/auth/constants.ts
+++
@@ -1,5 +0,0 @@
-import { env } from 'process';
-
-export const jwtConstants = {
-    secret: env.JWT_SECRET,
-  };
